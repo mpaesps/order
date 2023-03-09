f7e37ae4049f5c6e3c52aa2213dd3ec16f285eb1
Mayara Paes <mpaesps133@gmail.com>
2023-03-09 14:09:46 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
+import javax.xml.transform.Source;
+import java.sql.SQLOutput;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<OrderItem> orderItemList = new ArrayList<>();
+        Client client = new Client("Alex Green", "alex@gmail.com", new Date(85, 2, 15));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Product product = new Product("TV", 1000.0);
+        Product product1 = new Product("Mouse", 40.0);
+        OrderItem orderItem = new OrderItem(1, 1000.0, product);
+        OrderItem orderItem2 = new OrderItem(2, 40.0, product1);
+
+        orderItemList.add(orderItem);
+        orderItemList.add(orderItem2);
+
+        Order order = new Order(new Date(), OrderStatus.DELIVERED, client, orderItemList);
+
+        System.out.println("Entre com os dados do cliente: ");
+        System.out.println("Name: " + client.getName());
+        System.out.println("Email: " + client.getEmail());
+        System.out.println("Data de Nascimento: " + dateFormat.format(client.getBirthDate()));
+        System.out.println("\nEntre com os dados do pedido: ");
+        System.out.println("Status: " + OrderStatus.PROCESSING);
+        System.out.print("Quantos items para este pedido? ");
+        int a = 0;
+        for (int i = 0; i < orderItemList.size(); i++) {
+            a = (i + 1);
+        }
+        System.out.print(a);
+
+        System.out.println();
+        int i = 1;
+        for (OrderItem d : orderItemList) {
+            System.out.println("\nEntre com os dados do produto #" + i++);
+            System.out.println("Nome: " + d.getProduct().getName());
+            System.out.println("Preço: " + d.getProduct().getPrice());
+            System.out.println("Quantidade: " + d.getQuantity());
+        }
+
+        System.out.println("\nSumário da Ordem: \n"  + order.getMoment() +
+                                                "\n" + order.getStatus()+
+                                                "\n" + order.getClient().getName() + "(" + dateFormat.format(order.getClient().getBirthDate())+") - " + order.getClient().getEmail() +
+                                                "\nOrder Items: \n" );
+
+            for (OrderItem d : orderItemList) {
+
+                System.out.print( d.getProduct().getName() + ", ");
+                System.out.print("$" + d.getProduct().getPrice()+ ", ");
+                System.out.print("Qtde.:" + d.getQuantity()+ ", ");
+                System.out.println("Subtotal: "+ d.getPrice()+"\n");
+            };
+        System.out.println("Total: $" + order.total());
     }
 }
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,2 +1,81 @@
-package entities;public class Order {
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private Date moment;
+    private OrderStatus status;
+    private Client client;
+    private List<OrderItem> orderItems;
+
+    public Order() {
+    }
+
+    public Order(Date moment, OrderStatus status, Client client, List<OrderItem> orderItems) {
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.orderItems = orderItems;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public void addItem(OrderItem item){
+        orderItems.add(item);
+    }
+    public void removeItem(OrderItem item){
+        orderItems.remove(item);
+    }
+
+    public double total(){
+        double total = 0;
+        for (OrderItem a: this.orderItems ){
+            total += a.subTotal();
+
+        }return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "moment=" + moment +
+                ", status=" + status +
+                ", client=" + client +
+                ", orderItems=" + orderItems +
+                '}';
+    }
 }
